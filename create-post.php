3d113bf0ce8d077baa3433c08e9d729356ac9df9
Sanyuaung <sanyuaung.ygn.mm@gmail.com>
2023-02-03 21:18:50 +0630

    delete post and reset AUTO_INCREMENT

--- create-post.php
+++ create-post.php
@@ -6,29 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP_CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <style>
-    body {
-        padding: 50px;
-    }
+body {
+    padding: 50px;
+}
 </style>
 <?php
 include 'connection.php';
 session_start();
-$nameError = '';
+$titleError = '';
 $descError = '';
 if (isset($_POST['post'])) {
-    $name = $_POST['name'];
+    $title = $_POST['title'];
     $desc = $_POST['desc'];
-    if (empty($name)) {
-        $nameError = "Name is required";
+    if (empty($title)) {
+        $titleError = "Title is required";
     }
     if (empty($desc)) {
         $descError = "Description is required";
     }
-    if (!empty($name) && !empty($desc)) {
-        $query = "INSERT INTO `post`(`name`, `des`) VALUES ('$_POST[name]','$_POST[desc]')";
+    if (!empty($title) && !empty($desc)) {
+        $query = "INSERT INTO `post`(`title`, `description`) VALUES ('$_POST[title]','$_POST[desc]')";
         $insert = mysqli_query($db, $query);
         $_SESSION['message'] = "Post is created";
         header('location: index.php');
@@ -54,13 +55,15 @@ if (isset($_POST['post'])) {
                         <div class="card-body">
                             <form class="needs-validation" novalidate action="create-post.php" method="POST">
                                 <div class="mb-3">
-                                    <label for="" class="form-label">Name</label>
-                                    <input name="name" type="text" class="form-control is-invalid" id="" placeholder="Enter your post name">
-                                    <span class="text-danger invalid-feedback"><?= $nameError; ?></span>
+                                    <label for="" class="form-label">Title</label>
+                                    <input name="title" type="text" class="form-control is-invalid" id=""
+                                        placeholder="Enter your post name">
+                                    <span class="text-danger invalid-feedback"><?= $titleError; ?></span>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class="form-label">Description</label>
-                                    <input name="desc" type="text" class="form-control is-invalid" id="" placeholder="Enter your post description">
+                                    <input name="desc" type="text" class="form-control is-invalid" id=""
+                                        placeholder="Enter your post description">
                                     <span class="text-danger invalid-feedback"><?= $descError; ?></span>
                                 </div>
                                 <button name="post" type="submit" class="btn btn-primary">Post</button>
@@ -71,7 +74,9 @@ if (isset($_POST['post'])) {
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -6,37 +6,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP_CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <style>
-    body {
-        padding: 50px;
-    }
+body {
+    padding: 50px;
+}
 </style>
 <?php
 include 'connection.php';
 session_start();
 $query = "SELECT * FROM post";
 $select = mysqli_query($db, $query);
-
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    if ($id) {
+        $query="DELETE FROM post WHERE id=$id";
+        $delete = mysqli_query($db, $query);
+        $d=mysqli_query($db," ALTER TABLE post AUTO_INCREMENT = $id") ;
+        $_SESSION['message'] = "Deleted successfully.";
+        header("location: index.php");
+    }
+}
 ?>
 
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <?php if (isset($_SESSION['message'])) : ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <?php
+                        echo $_SESSION['message'];
+                        unset($_SESSION['message']);
+                    ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php endif ?>
                 <div class="card">
                     <div class="card-header">
                         <div class="row card-header">
-                            <?php if (isset($_SESSION['message'])) : ?>
-                                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                    <?php
-                                    echo $_SESSION['message'];
-                                    unset($_SESSION['message']);
-                                    ?>
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                </div>
-                            <?php endif ?>
                             <div class="col-6">
                                 <h1 class="text-primary ">Post Lists</h1>
                             </div>
@@ -49,7 +59,7 @@ $select = mysqli_query($db, $query);
                                 <thead>
                                     <tr>
                                         <th scope="col">ID</th>
-                                        <th scope="col">Name</th>
+                                        <th scope="col">Title</th>
                                         <th scope="col">Description</th>
                                         <th scope="col">Date & Time</th>
                                         <th scope="col">Action</th>
@@ -57,18 +67,17 @@ $select = mysqli_query($db, $query);
                                 </thead>
                                 <tbody>
                                     <?php foreach ($select as $a) : ?>
-                                        <tr>
-                                            <th scope="row"><?= $a['id']; ?></th>
-                                            <td><?= $a['name']; ?></td>
-                                            <td><?= $a['des']; ?></td>
-                                            <td><?= $a['create']; ?></td>
-                                            <td>
-                                                <a href="index.php?edit=<?php echo $a['id']; ?>" class="btn btn-danger">Edit</a>
-
-                                                <a name='delete' href="http://" class="btn btn-danger">Delete</a>
-                                                <a href="http://" class="btn btn-success">Update</a>
-                                            </td>
-                                        </tr>
+                                    <tr>
+                                        <th scope="row"><?= $a['id']; ?></th>
+                                        <td><?= $a['title']; ?></td>
+                                        <td><?= $a['description']; ?></td>
+                                        <td><?= $a['date']; ?></td>
+                                        <td>
+                                            <a name='delete' href="index.php?delete=<?php echo $a['id']; ?>" class=" btn
+                                                btn-danger">Delete</a>
+                                            <a href="http://" class="btn btn-success">Update</a>
+                                        </td>
+                                    </tr>
                                     <?php endforeach; ?>
 
                                 </tbody>
@@ -79,7 +88,9 @@ $select = mysqli_query($db, $query);
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
