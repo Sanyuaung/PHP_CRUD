623d9911b4ff97daecb4cad4eeaa46ef8f755ee0
Sanyuaung <sanyuaung.ygn.mm.com>
2023-02-03 09:24:53 +0630

    index.php design

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<style>
+    body {
+        padding: 50px;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row card-header">
+                            <div class="col-6">
+                                <h1 class="text-primary ">Post Lists</h1>
+                            </div>
+                            <div class="col-6">
+                                <a href="" class="btn btn-primary float-end">+ Create Post</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <table class="table card-subtitle">
+                                <thead>
+                                    <td>ID</td>
+                                    <td>Name</td>
+                                    <td>Description</td>
+                                    <td>Action</td>
+                                    </th>
+                                </thead>
+                                <tbody>
+                                    <th>1</th>
+                                    <th>PHP </th>
+                                    <th>afdaf </th>
+                                    <th>
+                                        <a href="http://" class="btn btn-danger">Delete</a>
+                                        <a href="http://" class="btn btn-success">Update</a>
+                                    </th>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
